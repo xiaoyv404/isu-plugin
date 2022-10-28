7be638a724922c06aa1719910065e4f6d585391f
xiaoyv404 <xiaoyv_404@outlook.com>
2022-10-28 15:44:24 +0800

    [修复]管理员判断

--- src/main/kotlin/app/Report.kt
+++ src/main/kotlin/app/Report.kt
@@ -7,27 +7,29 @@ import top.xiaoyv404.isu.*
 fun report() {
     GlobalEventChannel.subscribeGroupMessages {
         case("^今日考勤") {
-            if (PluginConfig.base.adminID.binarySearch(sender.id) != -1) {
-                if(PluginData.memberList.isEmpty()){
-                    group.sendMessage("唔，没有数据哦")
-                    return@case
-                }
-                val msg = StringBuilder()
-                msg.append("今日未出勤的猫猫有：")
-                var status = true // 是否都出勤
-                PluginData.memberList.forEach { (k, v) ->
-                    if (!v) {
-                        status = false
-                        msg.append("$k,  ")
-                    }
-                }
-                group.sendMessage(
-                    if (status)
-                        "猫猫今日已全部出勤"
-                    else
-                        msg.toString()
-                )
+            if (PluginConfig.base.adminID.find { it == sender.id } == null)
+                return@case
+
+            if (PluginData.memberList.isEmpty()) {
+                group.sendMessage("唔，没有数据哦")
+                return@case
             }
+
+            val msg = StringBuilder()
+            msg.append("今日未出勤的猫猫有：")
+            var status = true // 是否都出勤
+            PluginData.memberList.forEach { (k, v) ->
+                if (!v) {
+                    status = false
+                    msg.append("$k,  ")
+                }
+            }
+            group.sendMessage(
+                if (status)
+                    "猫猫今日已全部出勤"
+                else
+                    msg.toString()
+            )
         }
     }
 }
